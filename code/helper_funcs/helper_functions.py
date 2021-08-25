ef6090decc0f5b07dc498e56e70cd1b75784e2a7
phuongngo2904 <74227710+phuongngo2904@users.noreply.github.com>
2021-08-24 17:12:48 -0700

    Update helper_functions.py

--- code/helper_funcs/helper_functions.py
+++ code/helper_funcs/helper_functions.py
@@ -37,3 +37,30 @@ def test_model_w_images(model, TEST_DIR):
     predicted = labels[prediction.argmax()]
     print('Actual class: {} \n Predicted class: {}'.format(actual, predicted)) 
     plt.show()
+    
+def plot_loss_curves(history):
+  """
+  Returns separate loss curves for training and validation metrics.
+  """ 
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  accuracy = history.history['accuracy']
+  val_accuracy = history.history['val_accuracy']
+
+  epochs = range(len(history.history['loss']))
+
+  # Plot loss
+  plt.plot(epochs, loss, label='training_loss')
+  plt.plot(epochs, val_loss, label='val_loss')
+  plt.title('Loss')
+  plt.xlabel('Epochs')
+  plt.legend()
+
+  # Plot accuracy
+  plt.figure()
+  plt.plot(epochs, accuracy, label='training_accuracy')
+  plt.plot(epochs, val_accuracy, label='val_accuracy')
+  plt.title('Accuracy')
+  plt.xlabel('Epochs')
+  plt.legend();
